060423e77218b0291d7fc466fb52a74ad4a52cae
Shuhrat <holmatov999@gmail.com>
2026-08-15 18:56:39 +0400

    feat: implement URL shortening endpoint with validation

--- src/index.js
+++ src/index.js
@@ -14,6 +14,15 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.post("/api/shorten", async (req, res) => {
+    const {url} = req.body;
+    if (!url) {
+        return res.status(400).json( { error: "URL is required" });
+    }
+
+    
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
