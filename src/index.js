ebae0c5d488aa36eb2c27c686c125241db4ad5d3
Shuhrat <holmatov999@gmail.com>
2026-08-15 18:43:30 +0400

    fix: correct syntax errors in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
-const exprress = reuire("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
